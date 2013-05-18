0d1ae87e983898297c763de04f3f4d9f1e9b7b42
ben hengst <notbenh@cpan.org>
2013-05-17 21:21:11 -0700

    following https://github.com/sinatra/sinatra-recipes/blob/master/deployment/dreamhost_via_passenger.md

---
+++ public/config.ru
@@ -0,0 +1,3 @@
+require File.expand_path '../myapp.rb', __FILE__
+
+run Sinatra::Application
--- public/myapp.rb
+++ public/myapp.rb
@@ -1,6 +1,9 @@
-# myapp.rb
+# this is myapp.rb referred to above
 require 'sinatra'
-
 get '/' do
-  'Hello world!'
+  "Worked on dreamhost"
+end
+
+get '/foo/:bar' do
+  "You asked for foo/#{params[:bar]}"
 end
