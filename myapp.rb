8143e3302f8103f8be1d20050f064dd5358cd64b
ben hengst <notbenh@cpan.org>
2013-05-18 17:04:39 -0700

    have words working but roll is getting to me

--- myapp.rb
+++ myapp.rb
@@ -1,9 +1,26 @@
 require 'rubygems'
 require 'sinatra'
 get '/' do
-  "Worked on dreamhost"
+  "welcome to a whole new playground"
 end
 
-get '/foo/:bar' do
-  "You asked for foo/#{params[:bar]}"
+get '/pick/word/:word' do
+  words = [ %w[their there they're],
+            %w[too two to]
+          ].inject({}){ |hsh,ary|
+                        ary.inject(hsh){ |h,x|
+                                       h.merge(x => ary)
+                                     }
+                      }
+  words[params[:word]].sample
+end
+
+get '/roll/*' do
+  rolls = []
+  params[:splat].first.split('/').each do |dice|
+    max = dice.match(/^d(\d+)/).to_a.last
+    puts max
+    rolls.push(rand(1..max))
+  end
+  puts rolls
 end
