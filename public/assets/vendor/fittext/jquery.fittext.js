b780878a7e4f443c080c3723367db02cfbc28410
ben hengst <notbenh@cpan.org>
2013-09-19 21:54:47 -0700

    fire fire fire fire fire fire fire

--- public/assets/vendor/fittext/jquery.fittext.js
+++ public/assets/vendor/fittext/jquery.fittext.js
@@ -28,9 +28,18 @@
       // Resizer() resizes items based on the object width divided by the compressor * 10
       var resizer = function () {
         //$this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-        console.info("FITING:", $this.width()/(compressor*10) , parseFloat(settings.maxFontSize), parseFloat(settings.minFontSize))
-
-        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+        //console.info("FITING:", $this.width()/(compressor*10) , parseFloat(settings.maxFontSize), parseFloat(settings.minFontSize))
+        //console.info("DIMS:", $this.innerHeight(), $this.innerWidth())
+        calc = Math.min($this.width() / (compressor*10))
+        if( calc > 0) {
+          calc = Math.min(calc,parseFloat(settings.maxFontSize))
+        }
+        else{
+          calc = parseFloat(settings.maxFontSize)
+        }
+        pick = Math.max(calc, parseFloat(settings.minFontSize)) + 'px'
+        console.info('PICK:', pick)
+        $this.css('font-size', pick).css('line-height', pick )
       };
 
       // Call once to set.
