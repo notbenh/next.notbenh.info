3bf4f9236268816fd3ee06a348487869d07505c3
ben hengst <notbenh@cpan.org>
2013-09-19 20:18:28 -0700

    all sorts of stuff

--- public/assets/vendor/fittext/jquery.fittext.js
+++ public/assets/vendor/fittext/jquery.fittext.js
@@ -27,6 +27,9 @@
 
       // Resizer() resizes items based on the object width divided by the compressor * 10
       var resizer = function () {
+        //$this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+        console.info("FITING:", $this.width()/(compressor*10) , parseFloat(settings.maxFontSize), parseFloat(settings.minFontSize))
+
         $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
       };
 
